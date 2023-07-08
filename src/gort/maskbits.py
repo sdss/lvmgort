0a1dd6226ef05e0a5755280018a05c94a95f1ea1
José Sánchez-Gallego <gallegoj@uw.edu>
2023-07-08 15:29:04 +0000

    Add guider flags

---
+++ src/gort/maskbits.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2023-07-08
+# @Filename: maskbits.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+from __future__ import annotations
+
+from enum import Flag
+
+
+__all__ = ["GuiderStatus"]
+
+
+class GuiderStatus(Flag):
+    """Maskbits with the guider status."""
+
+    IDLE = 1 << 0
+    ACQUIRING = 1 << 1
+    GUIDING = 1 << 2
+    EXPOSING = 1 << 3
+    PROCESSING = 1 << 4
+    CORRECTING = 1 << 5
+    STOPPING = 1 << 6
+    FAILED = 1 << 7
+    WAITING = 1 << 8
+
+    NON_IDLE = (
+        ACQUIRING | GUIDING | EXPOSING | PROCESSING | CORRECTING | STOPPING | WAITING
+    )
+
+    def get_names(self):
+        """Returns a list of active bit names."""
+
+        return [bit.name for bit in GuiderStatus if self & bit and bit.name]
+
+    def __repr__(self):
+        return str(" | ".join(self.get_names()))
