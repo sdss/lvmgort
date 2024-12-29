9fbff736da3d6591cbde73f6fb807b2b0363393f
José Sánchez-Gallego <gallegoj@uw.edu>
2024-12-29 15:08:57 -0800

    Check actors in pre-observing recipe

--- src/gort/recipes/operations.py
+++ src/gort/recipes/operations.py
@@ -16,6 +16,7 @@ from rich.prompt import Confirm
 
 from sdsstools.utils import GatheringTaskGroup
 
+from gort.overwatcher.helpers import get_failed_actors, restart_actors
 from gort.tools import get_lvmapi_route, overwatcher_is_running
 
 from .base import BaseRecipe
@@ -292,8 +293,18 @@ class PreObservingRecipe(BaseRecipe):
 
     name = "pre-observing"
 
-    async def recipe(self):
+    async def recipe(self, check_actors: bool = True):
         """Runs the pre-observing sequence."""
+
+        if check_actors:
+            self.gort.log.info("Checking actors.")
+            failed_actors = await get_failed_actors(disacard_disabled=True)
+            if len(failed_actors) > 0:
+                self.gort.log.warning(f"Failed to ping actors: {failed_actors}.")
+                self.gort.log.info("Restarting actors.")
+                await restart_actors(list(failed_actors), self.gort)
+            else:
+                self.gort.log.info("All actors are pinging.")
 
         # Run a clean-up first in case there are any issues with the specs.
         await self.gort.cleanup(readout=False)
